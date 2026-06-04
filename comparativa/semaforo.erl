4253fdc179ff87e89b845f43dab79b2b67c0ec90
ignacvpr <111369053+ignacvpr@users.noreply.github.com>
2026-06-04 10:47:05 -0300

    inicio exploracion sintaxis Erlang, documento conceptos basicos del modulo

--- comparativa/semaforo.erl
+++ comparativa/semaforo.erl
@@ -1,2 +1,9 @@
-% Sistema de Semáforos Inteligentes
-% Grupo 39 — Paradigmas y Lenguajes 2026
+%explorando sintaxis basica de de erlang
+
+% -module indica el nombre del modulo, debe coincidir con el nombre del archivo
+-module(semaforo).
+
+% -export indica que funciones están disponibles para usar desde afuera
+% transicion/2 significa que la funcion transicion recibe 2 parametros
+-export([transicion/2]).
+
