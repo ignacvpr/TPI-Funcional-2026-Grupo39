c1b6582327c4293c5dc34666cce044fe29b6ed1f
cruzperez05 <cruzrollheiser@gmail.com>
2026-06-06 21:15:26 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,2 +1,17 @@
 ;; Sistema de Semáforos Inteligentes
 ;; Grupo 39 — Paradigmas y Lenguajes 2026
+
+
+;; ========================================================
+;; FUNCIÓN: transicion
+;; NATURALEZA: Pura 
+;; ESTRATEGIA: 
+;; IMPACTO: 
+;; ========================================================
+(defun transicion (color-actual cambiar-a) 
+  (cond
+    ((and (eq color-actual 'en-rojo)     (eq cambiar-a 'verde))  ;;
+     (list color-actual "cambiar-a-verde"))
+    ((and (eq color-actual 'en-verde)    (eq cambiar-a 'amarillo))
+     (list color-actual "cambiar-a-amarillos"))
+   
