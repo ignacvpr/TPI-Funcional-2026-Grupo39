c1a6eb61d3c0b15506d7453efe32aa86c437b478
cruzperez05 <cruzrollheiser@gmail.com>
2026-06-09 07:54:50 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -10,8 +10,11 @@
 ;; ========================================================
 (defun transicion (color-actual cambiar-a) 
   (cond
-    ((and (eq color-actual 'en-rojo)     (eq cambiar-a 'verde))  ;;
+    ((and (eq color-actual 'en-rojo)     (eq cambiar-a 'verde)) 
      (list color-actual "cambiar-a-verde"))
     ((and (eq color-actual 'en-verde)    (eq cambiar-a 'amarillo))
-     (list color-actual "cambiar-a-amarillos"))
-   
+     (list color-actual "cambiar-a-amarillo"))
+    ((and (eq color-actual 'en-amarillo) (eq cambiar-a 'rojo))
+     (list color-actual "cambiar-a-rojo"))
+    (t
+     (list color-actual 'accion-por-defecto))))
