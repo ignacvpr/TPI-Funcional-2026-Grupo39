9f7b35408107277bab757681ab3b71575fdb2774
cruzperez05 <cruzrollheiser@gmail.com>
2026-06-11 19:59:16 -0300

    Update core.lisp

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -4,34 +4,61 @@
 
 ;; ========================================================
 ;; FUNCIÓN: transicion
-;; NATURALEZA: Pura 
-;; ESTRATEGIA: 
-;; IMPACTO: 
+;; NATURALEZA: Pura (siempre el mismo
+;;             resultado. No imprime ni modifica nada mas)
+;; ESTRATEGIA: Función Predicado
+;; IMPACTO: No destructiva.
 ;; ========================================================
+
+;;recibe el estado actual y al color que se quiere ir
+;;retorna uan lista con el estado y una accion a realizar
 (defun transicion (color-actual cambiar-a) 
   (cond
-    ((and (eq color-actual 'en-rojo)     (eq cambiar-a 'verde)) 
+
+    ;; comprueba si esta en rojo y quiere ir a verde
+    ((and (equal color-actual 'en-rojo)     (equal cambiar-a 'verde)) ;; equal compara si dos simbolos son exactamente el mismo
      (list color-actual "cambiar-a-verde"))
-    ((and (eq color-actual 'en-verde)    (eq cambiar-a 'amarillo))
+
+    ;;comprueba si esta en verde y quiere ir a amarillo
+    ((and (equal color-actual 'en-verde)    (equal cambiar-a 'amarillo))
      (list color-actual "cambiar-a-amarillo"))
-    ((and (eq color-actual 'en-amarillo) (eq cambiar-a 'rojo))
+
+    ;; comprueba si esta en amarillo y quiere i a rojo
+    ((and (equal color-actual 'en-amarillo) (equal cambiar-a 'rojo))
      (list color-actual "cambiar-a-rojo"))
+
+    ;;retorna color actual y 'accion-por-defecto si la transición no es válida
     (t
      (list color-actual 'accion-por-defecto))))
 
 
 
-   ;; ========================================================
+
+
+;; ========================================================
 ;; FUNCIÓN: timer
-;; NATURALEZA: 
-;; ESTRATEGIA: 
+;; NATURALEZA: Pura — mismo timestamp siempre devuelve
+;;             el mismo color. No modifica nada externo.
+;; ESTRATEGIA: Condicional simple
 ;; IMPACTO: No destructiva
 ;; ========================================================
+
+;; recibe un tiempo Unix y devuelve el color correspondiente al momento especifico
 (defun timer (tiempo-unix)
+
+  ;; calcula en que punto del ciclo de 216 segundos estamos
   (let ((posicion (mod tiempo-unix 216)))
+
+    ;; comparamos la pocision con el rango de cada color 
     (cond
+
+      ;; 0 a 89 segundos, rojo (dura 90 segundos)
       ((< posicion 90)  'en-rojo)
+
+      ;; 90 a 95 segundos, amarillo (dura 6 segundos)
       ((< posicion 96)  'en-amarillo)
+
+      ;; 96 a 215 segundos, verde (dura 120 segundos)
       (t  'en-verde)
       )
     )
